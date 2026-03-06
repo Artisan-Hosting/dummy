3d1a4fef55e5a5c30b787982d7f3ace9feb36c16
Darrion Whitfield <dwhitfield@ramfield.net>
2026-03-05 23:39:13 -0600

    _: Sync time testing

--- test.sh
+++ test.sh
@@ -1,2 +1,4 @@
-rm -rfv *1* *2* *3* *4* *5* *6* *7* *8* *9* *0*
-./file.sh && git stage --all && git commit -m "_: Sync time testing" && git push
+#!/bin/bash 
+
+rm -rfv ./*1* ./*2* ./*3* ./*4* ./*5* ./*6* ./*7* ./*8* ./*9* ./*0*
+bash file.sh && git stage --all && git commit -m "_: Sync time testing" && git push
